9410ba9113a15f23049b752c7942220bcad4ad0d
cleiton langa <cleytonlanga10@gmail.com>
2025-09-06 15:40:09 +0200

    project configuration, app.js, server.js, db.js and user model.

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import dotenv from "dotenv";
+import sequelize from "./db.js";
+import app from "./app.js";
+import User from "./models/user.models.js";
+
+dotenv.config();
+const PORT = process.env.DB_PORT || 8080;
+
+
+// Start the server
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+    await sequelize.sync({ force: false });
+    console.log("All models were synchronized successfully.");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+})();
