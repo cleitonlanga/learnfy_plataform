e549af4c550bbafe52271484383fbbdaed113ebb
cleiton langa <cleytonlanga10@gmail.com>
2025-09-06 16:23:04 +0200

    creating video model

---
+++ backend/src/models/video.models.js
@@ -0,0 +1,37 @@
+import sequelize from "../config/db.js";
+import { DataTypes } from "sequelize";
+import User from "./user.models.js";
+
+const Video = sequelize.define("Video", {
+  id: {
+    type: DataTypes.UUID,
+    primaryKey: true,
+    defaultValue: DataTypes.UUIDV4,
+  },
+  userid: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: User,
+      key: "id",
+    },
+  },
+  youtubeUrl: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  status: {
+    type: DataTypes.ENUM("pending", "processed", "failed"),
+    allowNull: false,
+    defaultValue: "pending",
+  },
+  duration: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW,
+  },
+});
