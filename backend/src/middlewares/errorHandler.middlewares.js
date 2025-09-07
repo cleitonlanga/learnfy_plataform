3fe5dd2040421129aa49686e8c49a2dbb2d79598
cleiton langa <cleytonlanga10@gmail.com>
2025-09-07 19:33:00 +0200

    notFound and errors handlers.

---
+++ backend/src/middlewares/errorHandler.middlewares.js
@@ -0,0 +1,24 @@
+export const errorHandler = () => {
+  return (err, req, res, next) => {
+    try {
+      console.error("Error:", err);
+      const statusCode = err.statusCode || 500;
+      const message = err.message || "Internal Server Error";
+      return res.status(statusCode).json({
+        message,
+        stack: process.env.NODE_ENV === "development" ? err.stack : null,
+      });
+    } catch (error) {
+      console.error("Error in error handler middleware:", error);
+      return res.status(500).json({ message: "Internal server error" });
+    }
+  };
+};
+
+export const notFound = () => {
+  return (req, res, next) => {
+    const error = new Error(`Not Found - ${req.originalUrl}`);
+    res.status(404);
+    next(error);
+  };
+};
