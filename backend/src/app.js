e12f4176a4667cb60cd96a5e7b4f4b89e3be75c5
cleiton langa <cleytonlanga10@gmail.com>
2025-09-07 19:57:35 +0200

    #

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
-
+import {
+  errorHandler,
+  notFound,
+} from "./middlewares/errorHandler.middlewares.js";
 
 const app = express();
 app.use(cors());
@@ -10,5 +13,10 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
+
+
+//Middlewares
+app.use(notFound());
+app.use(errorHandler());
 
 export default app;
