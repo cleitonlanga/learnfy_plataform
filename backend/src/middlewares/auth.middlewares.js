2c221d9b00de66ad034143bb8c8f4e4f69e2e67d
cleiton langa <cleytonlanga10@gmail.com>
2025-09-11 00:45:59 +0200

    added attributes to exclud password

--- backend/src/middlewares/auth.middlewares.js
+++ backend/src/middlewares/auth.middlewares.js
@@ -14,7 +14,9 @@ export const protect = (roles = []) => {
       const token = authHeader.split(" ")[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-      const user = await User.findByPk(decoded.id).select("-password");
+      const user = await User.findByPk(decoded.id, {
+        attributes: { exclude: ["password"] },
+      });
       if (!user) {
         return res
           .status(401)
@@ -29,6 +31,9 @@ export const protect = (roles = []) => {
       next();
     } catch (error) {
       console.error("Error in auth middleware:", error);
+      if (error.name === "TokenExpiredError") {
+        return res.status(401).json({ message: "Token expired" });
+      }
       return res.status(401).json({ message: "Not authorized, token failed" });
     }
   };
