c5ea7e73c5ceed1aeb941afb1e4d825f3d51ca3a
cleiton langa <cleytonlanga10@gmail.com>
2025-10-01 14:13:34 +0200

    google auth created

---
+++ backend/src/config/passport.js
@@ -0,0 +1,47 @@
+import dotenv from "dotenv";
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import User from "../models/user.models.js";
+
+dotenv.config();
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_REDIRECT_URI,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        let user = await User.findOne({ where: { googleId: profile.id } });
+
+        if (!user) {
+          user = await User.create({
+            username: profile.displayName,
+            email: profile.emails[0].value,
+            googleId: profile.id,
+            password: null,
+            role: "user",
+          });
+        }
+
+        return done(null, user);
+      } catch (err) {
+        return done(err, null);
+      }
+    }
+  )
+);
+
+// serialização
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  const user = await User.findById(id);
+  done(null, user);
+});
+
+export default passport;
