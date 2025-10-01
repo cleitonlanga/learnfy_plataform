955e4ab5347a23225f9957410ea5f9fd8807ac6a
cleiton langa <cleytonlanga10@gmail.com>
2025-10-01 14:12:07 +0200

    added google auth passport

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,10 +6,12 @@ import {
 } from "./middlewares/errorHandler.middlewares.js";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
+import passport from "./config/passport.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(passport.initialize());
 
 //Routes
 app.use("/api/auth", authRoutes);
