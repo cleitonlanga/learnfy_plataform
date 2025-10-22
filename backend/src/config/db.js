68b482e9e4a01e38eb3a4fddc55c5287da2b2cdb
cleiton langa <cleytonlanga10@gmail.com>
2025-10-23 00:30:50 +0200

    just wanted to remake the db

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -11,7 +11,7 @@ const sequelize = new Sequelize(
     host: process.env.DB_HOST,
     dialect: "mariadb",
     port: process.env.DB_PORT,
-    logging: false,
+    logging: false,// logging is only true for development 
   }
 );
 
